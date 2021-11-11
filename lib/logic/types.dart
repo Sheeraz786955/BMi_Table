88d4295faf4247d34445bdf1a54db426e74355cc
Sheeraz786955 <59965006+Sheeraz786955@users.noreply.github.com>
2021-11-11 11:08:39 +0500

    ok

---
+++ lib/logic/types.dart
@@ -0,0 +1,10 @@
+const double MAX_HEIGHT = 250;
+const double MIN_HIEGHT = 1;
+const CENTIMETER = '';
+const KILOGRAM = "";
+const AGEUNIT = "";
+enum GenderType {MALE,FEMALE}
+
+//const OVERWEIGHT = "overweight";
+//const NORMAL = "normal";
+//const UNDERWEIGHT = "underweight";
